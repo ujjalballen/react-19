8b95813645cfb47c3b354338e8a43f6e119b5fd0
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-11-19 14:13:49 +0600

    useFormState but its change/rename in react 19

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { BasicUsage } from "./components/useStateForm/basic-usage";
+// import { DataUsage } from "./components/useStateForm/data-usage";
+// import { Pitfall } from "./components/useStateForm/pitfall";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      {/* <Pitfall /> */}
+    </div>
+  );
+}
+
+export default App;
